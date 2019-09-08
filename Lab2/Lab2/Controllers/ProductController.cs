f8ba4a092c12b67eb2687ec9d979d469d89d52fa
AlecCCC <S01146145@acad.tri-c.edu>
2019-09-08 18:02:43 -0400

    Lab2

---
+++ Lab2/Lab2/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Index()
+        {
+            return "Product/Index Is Displayed";
+        }
+
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+
+
+        public string Details(int ID)
+        {
+            return "Details displayed for Id=" + ID;
+
+        }
+
+
+        public string Location(int zip)
+        {
+            string message = HttpUtility.HtmlEncode("Location displayed for zip=" + zip);
+            return message;
+        }
+
+    }
+
+}
