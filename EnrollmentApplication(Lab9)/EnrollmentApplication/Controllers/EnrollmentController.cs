6f4a3f694fd414a33fb5d4959803e53b3270899e
AlecCCC <S01146145@acad.tri-c.edu>
2019-10-27 20:42:34 -0400

    Lab9 Done

---
+++ EnrollmentApplication(Lab9)/EnrollmentApplication/Controllers/EnrollmentController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EnrollmentApplication.Models;
+
+namespace EnrollmentApplication.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+        private EnrollmentDB db = new EnrollmentDB();
+
+        public ActionResult MonthStudent()
+        {
+
+            var Student = GetMonthStudent();
+            return PartialView("_MonthStudent", Student);
+        }
+
+        private object GetMonthStudent()
+        {
+            var Student = db.Students
+                .OrderBy(a => System.Guid.NewGuid())
+                .First();
+            return Student;
+
+        }
+
+
+        public ActionResult StudentSearch(string q)
+        {
+            var FirstName = GetStudents(q);
+            return PartialView("_StudentSearch", FirstName);
+        }
+
+        private List<Student> GetStudents(string searchString)
+        {
+            return db.Students
+            .Where(a => a.FirstName.Contains(searchString))
+            .ToList();
+        }
+
+        /// /////////////////////////////////////////////////////////////////////////
+
+        public ActionResult CourseSearch(string q)
+        {
+            var courses = GetCourses(q);
+            return PartialView("_CourseSearch", courses);
+        }
+
+        private List<Course> GetCourses(string searchString)
+        {
+            return db.Courses
+            .Where(a => a.CourseTitle.Contains(searchString))
+            .ToList();
+        }
+
+        // GET: Enrollment
+        public ActionResult Index()
+        {
+            var enrollments = db.Enrollments.Include(e => e.Course).Include(e => e.Student);
+            return View(enrollments.ToList());
+        }
+
+        // GET: Enrollment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "CourseTitle");
+            ViewBag.StudentId = new SelectList(db.Students, "StudentId", "LastName");
+            return View();
+        }
+
+        // POST: Enrollment/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EnrollmentId,StudentId,CourseId,Grade,Notes")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollments.Add(enrollment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "CourseTitle", enrollment.CourseId);
+            ViewBag.StudentId = new SelectList(db.Students, "StudentId", "LastName", enrollment.StudentId);
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "CourseTitle", enrollment.CourseId);
+            ViewBag.StudentId = new SelectList(db.Students, "StudentId", "LastName", enrollment.StudentId);
+            return View(enrollment);
+        }
+
+        // POST: Enrollment/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EnrollmentId,StudentId,CourseId,Grade,Notes")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "CourseTitle", enrollment.CourseId);
+            ViewBag.StudentId = new SelectList(db.Students, "StudentId", "LastName", enrollment.StudentId);
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = db.Enrollments.Find(id);
+            db.Enrollments.Remove(enrollment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EnrollmentApplication(Lab9)/EnrollmentApplication/Models/Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace EnrollmentApplication.Models
+{
+    public class Student : IValidatableObject
+    {
+
+        [Required(ErrorMessage = "You need to enter a student ID.")]
+        [Display(Name = "Student ID")]
+        public virtual int StudentId { get; set; }
+
+        [Required(ErrorMessage = "You need to enter a student Last Name.")]
+        [StringLength(50, ErrorMessage = "Last name is too long.")]
+        [Display(Name = "Last Name")]
+
+        public virtual string LastName { get; set; }
+
+        [Required(ErrorMessage = "You need to enter a student First Name.")]
+        [StringLength(50, ErrorMessage = "First name is too long.")]
+        [Display(Name = "First Name")]
+        public virtual string FirstName { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public string State { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+         
+            var pAddress2 = new[] { "Address2" };
+            if(Address2 == Address1)
+                
+            {
+
+                {
+                   yield return (new ValidationResult("Address 2 cannot be the same as Address 1",pAddress2));
+                }
+
+            }
+
+            var pState = new[] { "State" };
+            int statelength = State.Length;
+
+            if (State.Length != 2 )
+            {
+
+                {
+                    yield return (new ValidationResult("Enter a 2 digit State code", pState));
+                }
+            }
+
+            var pZipcode = new[] { "Zipcode" };
+            
+            int zipcodelength = Zipcode.Length;
+            
+            if (Zipcode.Length != 5)
+            {
+
+                {
+                    yield return (new ValidationResult("Enter a 5 digit Zipcode", pZipcode));
+                }
+            }
+
+            throw new NotImplementedException();
+       
+        }
+    }
+}
